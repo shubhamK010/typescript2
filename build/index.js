6d3476d5a76c6edc61c99ce12ecfc614a73f88d6
github_shubhamK010 <shubhamkmb@gmail.com>
2022-09-11 20:44:12 +0530

    dayOne

---
+++ build/index.js
@@ -0,0 +1,23 @@
+"use strict";
+// let x:number = 10
+// console.log(x)
+// let firstName:string = "shubham"
+// console.log(firstName)
+// //Program 1
+// function add(x:number,y:number){
+//     // document.write(x+y)
+//     console.log(x+y)
+// }
+// add(1,2)
+//Program 2
+let firstName = "pravin";
+let x = 10;
+let y = true;
+let a = {
+    firstName: "bapurao",
+    lastName: "shelar"
+};
+let x1 = [1, 2, 3, 4, 5]; //here only number values are needed
+let x2 = ["pravin", "tushar", "bapurao"];
+console.log(x1);
+console.log(x2);
---
+++ src/index.ts
@@ -0,0 +1,33 @@
+// let x:number = 10
+// console.log(x)
+
+// let firstName:string = "shubham"
+// console.log(firstName)
+
+
+
+// //Program 1
+// function add(x:number,y:number){
+    
+//     // document.write(x+y)
+//     console.log(x+y)
+
+// }
+// add(1,2)
+
+
+//Program 2
+
+let firstName:string = "pravin"
+let x:number = 10
+let y:boolean = true
+let a:object ={
+    firstName:"bapurao",
+    lastName:"shelar"
+}
+
+let x1:number[] =[1,2,3,4,5] //here only number values are needed
+let x2:string[] = ["pravin","tushar","bapurao"]
+
+console.log(x1)
+console.log(x2)
